bf4039be664a1fca22fb17e365f8db00c000ca1e
Kevin Clement <kevin.clement@gmail.com>
2019-09-30 21:18:12 -0700

    updating to live version

--- museum-peephole.ino
+++ museum-peephole.ino
@@ -1,5 +1,9 @@
 const int BTN_1_PIN = 9;
 const int BTN_2_PIN = 6;
+
+const int LED_BTN_1_PIN = 7;
+const int LED_BTN_2_PIN = 8;
+
 const int LED_1_PIN = 10;
 const int LED_2_PIN = 16;
 const int LED_3_PIN = 14;
@@ -13,7 +17,10 @@ void setup() {
   pinMode(LED_1_PIN, OUTPUT);
   pinMode(LED_2_PIN, OUTPUT);
   pinMode(LED_3_PIN, OUTPUT);
-  
+
+  pinMode(LED_BTN_1_PIN, OUTPUT);
+  pinMode(LED_BTN_2_PIN, OUTPUT);
+
   pinMode(BTN_1_PIN, INPUT);
   pinMode(BTN_2_PIN, INPUT);
 }
@@ -22,22 +29,23 @@ void loop() {
   btn1 = digitalRead(BTN_1_PIN);
   btn2 = digitalRead(BTN_2_PIN);
 
+  // map led above button to state of button
+  digitalWrite(LED_BTN_1_PIN, btn1);
+  digitalWrite(LED_BTN_2_PIN, btn2);
+
   if (btn1 == HIGH && btn2 == LOW) {
-    Serial.println("1 - ON");
     digitalWrite(LED_1_PIN, HIGH);
   } else {
     digitalWrite(LED_1_PIN, LOW);
   }
 
   if (btn1 == HIGH && btn2 == HIGH) {
-    Serial.println("2 - ON");
     digitalWrite(LED_2_PIN, HIGH);
   } else {
     digitalWrite(LED_2_PIN, LOW);
   }
 
   if (btn2 == HIGH && btn1 == LOW) {
-//    Serial.println("3 - ON");
     digitalWrite(LED_3_PIN, HIGH);
   } else {
     digitalWrite(LED_3_PIN, LOW);
