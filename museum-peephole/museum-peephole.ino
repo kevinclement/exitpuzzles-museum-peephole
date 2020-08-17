32331f80af4176d33c042f80ab9cb911a06c5167
Kevin Clement <kevin.clement@gmail.com>
2020-08-16 19:36:37 -0700

    moving to platformio

---
+++ museum-peephole/museum-peephole.ino
@@ -0,0 +1,65 @@
+const int BTN_1_PIN = 9;
+const int BTN_2_PIN = 6;
+
+const int LED_BTN_1_PIN = 7;
+const int LED_BTN_2_PIN = 8;
+
+const int LED_FRNT_1_PIN = 21;
+const int LED_FRNT_2_PIN = 20;
+const int LED_FRNT_3_PIN = 19;
+
+const int LED_1_PIN = 10;
+const int LED_2_PIN = 16;
+const int LED_3_PIN = 14;
+
+int btn1 = 0;
+int btn2 = 0;
+void setup() {
+  Serial.begin(9600);
+  Serial.println("Museum Peephole by kevinc...");
+  
+  pinMode(LED_1_PIN, OUTPUT);
+  pinMode(LED_2_PIN, OUTPUT);
+  pinMode(LED_3_PIN, OUTPUT);
+
+  pinMode(LED_BTN_1_PIN, OUTPUT);
+  pinMode(LED_BTN_2_PIN, OUTPUT);
+
+  pinMode(LED_FRNT_1_PIN, OUTPUT);
+  pinMode(LED_FRNT_2_PIN, OUTPUT);
+  pinMode(LED_FRNT_3_PIN, OUTPUT);
+
+  pinMode(BTN_1_PIN, INPUT);
+  pinMode(BTN_2_PIN, INPUT);
+}
+
+void loop() {
+  btn1 = digitalRead(BTN_1_PIN);
+  btn2 = digitalRead(BTN_2_PIN);
+
+  // map led above button to state of button
+  digitalWrite(LED_BTN_1_PIN, btn1);
+  digitalWrite(LED_BTN_2_PIN, btn2);
+
+  digitalWrite(LED_FRNT_1_PIN, HIGH);
+  digitalWrite(LED_FRNT_2_PIN, HIGH);
+  digitalWrite(LED_FRNT_3_PIN, HIGH);
+
+  if (btn1 == HIGH && btn2 == LOW) {
+    digitalWrite(LED_1_PIN, HIGH);
+  } else {
+    digitalWrite(LED_1_PIN, LOW);
+  }
+
+  if (btn1 == HIGH && btn2 == HIGH) {
+    digitalWrite(LED_2_PIN, HIGH);
+  } else {
+    digitalWrite(LED_2_PIN, LOW);
+  }
+
+  if (btn2 == HIGH && btn1 == LOW) {
+    digitalWrite(LED_3_PIN, HIGH);
+  } else {
+    digitalWrite(LED_3_PIN, LOW);
+  }
+}
--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,14 @@
+#include <Arduino.h>
+
 const int BTN_1_PIN = 9;
 const int BTN_2_PIN = 6;
 
 const int LED_BTN_1_PIN = 7;
 const int LED_BTN_2_PIN = 8;
+
+const int LED_FRNT_1_PIN = 21;
+const int LED_FRNT_2_PIN = 20;
+const int LED_FRNT_3_PIN = 19;
 
 const int LED_1_PIN = 10;
 const int LED_2_PIN = 16;
@@ -10,16 +16,25 @@ const int LED_3_PIN = 14;
 
 int btn1 = 0;
 int btn2 = 0;
+
+void(* resetFunc) (void) = 0;
+
 void setup() {
   Serial.begin(9600);
+  Serial.setTimeout(10);
+  Serial.println("READY");
   Serial.println("Museum Peephole by kevinc...");
-  
+
   pinMode(LED_1_PIN, OUTPUT);
   pinMode(LED_2_PIN, OUTPUT);
   pinMode(LED_3_PIN, OUTPUT);
 
   pinMode(LED_BTN_1_PIN, OUTPUT);
   pinMode(LED_BTN_2_PIN, OUTPUT);
+
+  pinMode(LED_FRNT_1_PIN, OUTPUT);
+  pinMode(LED_FRNT_2_PIN, OUTPUT);
+  pinMode(LED_FRNT_3_PIN, OUTPUT);
 
   pinMode(BTN_1_PIN, INPUT);
   pinMode(BTN_2_PIN, INPUT);
@@ -32,6 +47,10 @@ void loop() {
   // map led above button to state of button
   digitalWrite(LED_BTN_1_PIN, btn1);
   digitalWrite(LED_BTN_2_PIN, btn2);
+
+  digitalWrite(LED_FRNT_1_PIN, HIGH);
+  digitalWrite(LED_FRNT_2_PIN, HIGH);
+  digitalWrite(LED_FRNT_3_PIN, HIGH);
 
   if (btn1 == HIGH && btn2 == LOW) {
     digitalWrite(LED_1_PIN, HIGH);
